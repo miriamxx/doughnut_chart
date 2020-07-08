fcfab6cbf4bd7ea2b5be38d5e600f16283251491
Miriam <miriam24729@gmail.com>
2020-07-07 21:39:00 -0400

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,35 @@
+module.exports = [{
+    label: 'Health',
+    value: 1
+},
+{
+    label: 'Productivity',
+    value: 10000
+},
+{
+    label: 'Mental Health',
+    value: 1900,
+    className: 'classNameTest'
+},
+{
+    label: 'Networked',
+    value: 800,
+ 
+},
+{
+    label: 'Career growth',
+    value: 760
+},
+{
+    label: 'Happiness',
+    value: 1108
+},
+{
+    label: 'Personal Development',
+    value: 500
+},
+
+{
+    label: 'Focused',
+    value: 1250
+}];
